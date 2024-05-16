adf58aa63b01603a555f17ffea630695b97d6734
Christopher Perez <cubanchris1@gmail.com>
2024-05-16 00:58:40 -0400

    initial stuff

---
+++ components/intro.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import Image from 'next/image';
+import React from 'react';
+import { motion } from 'framer-motion';
+import Link from "next/link";
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithubSquare } from 'react-icons/fa';
+
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+        <div className='flex items-center justify-center'>
+            <div className="relative">
+                <motion.div
+                    initial={{opacity:0, scale:0}}
+                    animate={{opacity:1, scale:1}}
+                    transition={{
+                        type: "tween",
+                        duration: 0.2,
+                    }}
+                >
+                    <Image 
+                    src="/chris.jpeg" 
+                    alt="Sexy photo of Chris" 
+                    width="384"
+                    height="384"
+                    quality={95}
+                    priority={true}
+                    className=" w-48 h-48 rounded-full border-[0.35rem] border-white shadow-xl object-cover"
+                    />
+                </motion.div>
+            
+
+            </div>
+
+        </div>
+
+        <motion.p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl "
+        initial={{opacity:0, y: 100}}
+        animate={{opacity:1, y: 0}}
+        >
+            Hey, I'm Chris. I'm a student at Harvard College studying Statistics and Computer Science. I'm passionate about leveraging machine learning, software engineering, and data science for social good. 
+        </motion.p>
+
+        <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium "
+            initial={{opacity:0, y: 100}}
+            animate={{opacity:1, y: 0}}
+            transition={{
+                delay:0.1,
+            }}
+        >
+            <a className="bg-white p-4 hover:text-gray-950 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10"
+            href="https://linkedin.com/in/cpez" target="_blank">
+                <BsLinkedin />
+            </a>
+            <a className="bg-white p-4 text-gray-700 flex items-center text-[1.35rem] gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+            href="https://github.com/cpez04" target="_blank">
+                <FaGithubSquare />
+            </a>
+        </motion.div>
+
+
+
+    </section>
+  )
+}
