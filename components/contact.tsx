8c3a77eb57f3b8a41401c0e269687ec74c5182e0
CHRISTOPHER PEREZ <CHRISTOPHERPEREZ@COLLEGE.HARVARD.EDU>
2024-12-24 22:06:17 -0500

    Update contact

--- components/contact.tsx
+++ components/contact.tsx
@@ -4,9 +4,6 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
-import { sendEmail } from "@/actions/sendEmail";
-import Submitbtn from "./submit-btn";
-import { toast } from "react-hot-toast";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact", 0.5);
@@ -23,44 +20,8 @@ export default function Contact() {
     >
       <SectionHeading>Contact Me</SectionHeading>
       <p className="text-gray-700 -mt-6 dark:text-white/80">
-        Please contact me directly at{" "}
-        <a
-          className="underline text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300"
-          href="mailto:christopherperez@college.harvard.edu"
-        >
-          christopherperez@college.harvard.edu
-        </a>{" "}
-        or through this form.
+        Please contact me directly at christopherperez[at]college[dot]harvard[dot]edu.
       </p>
-
-      <form
-        className="mt-10 flex flex-col dark:text-black space-y-4"
-        action={async (formData) => {
-          const { data, error } = await sendEmail(formData);
-          if (error) {
-            toast.error(error);
-            return;
-          }
-          toast.success("Email sent successfully!");
-        }}
-      >
-        <input
-          className="h-14 px-4 rounded-lg border border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200 dark:bg-gray-700 dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none dark:border-gray-600 dark:focus:border-blue-400"
-          type="email"
-          required
-          maxLength={500}
-          placeholder="Your Email"
-          name="senderEmail"
-        />
-        <textarea
-          className="h-52 px-4 py-3 rounded-lg border border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200 dark:bg-gray-700 dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none dark:border-gray-600 dark:focus:border-blue-400"
-          placeholder="Your Message"
-          required
-          maxLength={5000}
-          name="message"
-        />
-        <Submitbtn />
-      </form>
     </motion.section>
   );
 }
